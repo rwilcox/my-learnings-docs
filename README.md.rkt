0e6bbf711ee44e21b30d1b84786325e3366ac187
Ryan Wilcox <rwilcox@wilcoxd.com>
2021-10-10 16:05:08 -0400

    new readme file .rkt

---
+++ README.md.rkt
@@ -0,0 +1,24 @@
+#lang scribble/text
+
+Eventually I'm going to turn this documentation page into some kind of [Gatsby](https://www.gatsbyjs.com/) enabled static site.
+
+If you're looking at this repo directly, that day is not today.
+
+# Technologies used here
+
+The first approach for this site is to use Github as a (decent!) markdown rendering platform for individual files.
+
+Some files are preprocessed via [Racket's scribble library](https://docs.racket-lang.org/scribble/index.html) documentation tool. Then I checkin / commit the generated files so they are viewed via its rendered Github generated markdown
+
+# File Index
+
+Because with a README this is no longer auto-generated for me...
+
+@(define (display-file-list)
+   (map
+    (lambda (current) (string-append "  * [" (path->string current) "](https://github.com/rwilcox/my-learnings-docs/blob/master/" (path->string current) ")\n"))
+    (filter
+     (lambda (current) (string-suffix? (path->string current) ".md"))
+     (directory-list "." #:build? #f))))
+
+@(display-file-list)
