8cec9a87ba109860126b41110aead0dceae99b1b
Ryan Wilcox <rwilcox@wilcoxd.com>
2021-10-10 22:50:24 -0400

    got the sythesis about technical leadership rendering correctly

--- synthesis_technical_leadership.md.rkt
+++ synthesis_technical_leadership.md.rkt
@@ -1,3 +1,6 @@
+#lang scribble/text
+@(require "scribble-utils.rkt")
+
 ---
 path: "/synthesis/technical_leadership"
 title: "Synthesis:Technical Leadership"
@@ -77,16 +80,16 @@ Even at this point, what is your role? See SDLC_Roles
 
 # SDLC and business partners
 
-
-> 1. Align business and IT goals
-> 2. Define business improvement
-> 3. Design Business Change
->  
-> 4.Implement business change
->  
-> 5.Deliver business benefits
-> 
-> - From IT Enabled Business Change by Dr. Sharm Manwani on page 14 (https://learning.oreilly.com/library/view/it-enabled-business-change/9781902505916/9781902505916_ch02_sec01_02.html)
+@quote-highlight[
+	#:title       "IT Enabled Business Change"
+	#:author      "Dr. Sharm Manwani"
+	#:page-number 14
+	#:url         "https://learning.oreilly.com/library/view/it-enabled-business-change/9781902505916/9781902505916_ch02_sec01_02.html"]{
+  1. Align business and IT goals
+  2. Define business improvement
+  3. Design Business Change
+   4.Implement business change
+   5.Deliver business benefits}
 
 
 # Flowing Goals down
@@ -103,23 +106,22 @@ with those targets.
 
 ## Q: How much control after all??
 
+@quote-highlight[
+	#:title       "Fundamentals of Software Architecture"
+	#:author      "Mark Richards, Neal Ford"
+	#:page-number 22
+	#:url         "https://learning.oreilly.com/library/view/fundamentals-of-software/9781492043447/ch22.html"]{
+* Team familiarity
+How well do the team members know each other? Have they worked together before on a project? Generally, the better team members know each other, the less control is needed because team members start to become self-organizing.
+  * Team size
+How big is the team? (We consider more than 12 developers on the same team to be a big team, and 4 or fewer to be a small team.) The larger the team, the more control is needed
+  * Overall experience
+How many team members are senior? How many are junior? Is it a mixed team of junior and senior developers? How well do they know the technology and business domain? Teams with lots of junior developers require more control and mentoring, whereas teams with more senior developers require less control
+  * Project complexity
+Is the project highly complex or just a simple website? Highly complex projects require the architect to be more available to the team and to assist with issues that arise, hence more control is needed on the team. Relatively simple projects are straightforward and hence do not require much control.
 
-> * Team familiarity
-> How well do the team members know each other? Have they worked together before on a project? Generally, the better team members know each other, the less control is needed because team members start to become self-organizing.
->   
-> * Team size
-> How big is the team? (We consider more than 12 developers on the same team to be a big team, and 4 or fewer to be a small team.) The larger the team, the more control is needed
->   
-> * Overall experience
-> How many team members are senior? How many are junior? Is it a mixed team of junior and senior developers? How well do they know the technology and business domain? Teams with lots of junior developers require more control and mentoring, whereas teams with more senior developers require less control
->   
-> * Project complexity
-> Is the project highly complex or just a simple website? Highly complex projects require the architect to be more available to the team and to assist with issues that arise, hence more control is needed on the team. Relatively simple projects are straightforward and hence do not require much control.
->   
-> * Project duration
-> Is the project short (two months), long (two years), or average duration (six months)? The shorter the duration, the less control is needed; conversely, the longer the project, the more control is needed.
-> 
-> - From Fundamentals of Software Architecture by Mark Richards, Neal Ford on page 22 (https://learning.oreilly.com/library/view/fundamentals-of-software/9781492043447/ch22.html)
+  * Project duration
+Is the project short (two months), long (two years), or average duration (six months)? The shorter the duration, the less control is needed; conversely, the longer the project, the more control is needed.}
 
 ## your area of responsibility
 
@@ -184,14 +186,15 @@ In product organizations this may be the product owner???? (This May or May not 
 
 # MOI model of leadership
 
+@quote-highlight[
+	#:title       "Becoming a technical leader"
+	#:author      "Gerald Weinburg"
+	#:page-number 412
+	#:url         "https://www.amazon.com/Becoming-Technical-Leader-Gerald-Weinberg-ebook/dp/B004J4VV3I/ref=sr_1_1?crid=FHTWINGGX9HB&dchild=1&keywords=becoming+a+technical+leader&qid=1597625292&sprefix=becoming+a+techical+leader%2Caps%2C668&sr=8-1"]{MOI model of leadership
 
-> MOI model of leadership
-> * M: motivation
-> * O: organization
->  
-> * ideas / innovation
-> 
-> - From Becoming a technical leader by Gerald Weinburg on page 412 (https://www.amazon.com/Becoming-Technical-Leader-Gerald-Weinberg-ebook/dp/B004J4VV3I/ref=sr_1_1?crid=FHTWINGGX9HB&dchild=1&keywords=becoming+a+technical+leader&qid=1597625292&sprefix=becoming+a+techical+leader%2Caps%2C668&sr=8-1)
+  * M: motivation
+  * O: organization
+   * ideas / innovation}
 
 
 # - [ BOOKQUOTE]: misc leaderahip
@@ -218,50 +221,54 @@ first job: esablizh a comfortzbld environment, which means geftinf acquanted wit
 
 # Managing flow of ideas
 
+@quote-highlight[
+	#:title       "Becoming a technical leader"
+	#:author      "Gerald Weinburg"
+	#:page-number 1624
+	#:url         "https://www.amazon.com/Becoming-Technical-Leader-Gerald-Weinberg-ebook/dp/B004J4VV3I/ref=sr_1_1?crid=FHTWINGGX9HB&dchild=1&keywords=becoming+a+technical+leader&qid=1597625292&sprefix=becoming+a+techical+leader%2Caps%2C668&sr=8-1"]{managing flow of ideas
 
-> managing flow of ideas
-> * contribute a clever idea to the team
-> * encouraging copying of useful ideas
-> * elaborate on ideas that teammates contributed
-> * drop ones own idea in favor of an idea that someone wants to develop
-> * refuse to let an idea drop before everyone understands it
-> * resist time pressure and take time to listen when other people explain their ideas
-> * test ideas contributed by other people
-> * withhold quick critics of teammates ideas in order to keep the ideas flowing
-> * when crisising an idea make sure you are clear you are critiquing thr idea not the person
-> * test your own ideas before offering them
-> * when time and labor are short stop working on new ideas and just pitch in
-> * encourage team to drop ideas that succeeded earlier but will not any longer because of change
-> * revive a dropped idea when it will now work because of change
-> * does the idea add to, reduce, or keep the same the amount of toil on the project? (Can it be refined to be automatable? But sure, simple things that don’t scale have a place too... just maybe not if you’be scaled) (-RW)
-> * is it an idea that didn’t use to work, but now can? (-RW)
-> 
-> - From Becoming a technical leader by Gerald Weinburg on page 1624 (https://www.amazon.com/Becoming-Technical-Leader-Gerald-Weinberg-ebook/dp/B004J4VV3I/ref=sr_1_1?crid=FHTWINGGX9HB&dchild=1&keywords=becoming+a+technical+leader&qid=1597625292&sprefix=becoming+a+techical+leader%2Caps%2C668&sr=8-1)
+  * contribute a clever idea to the team
+  * encouraging copying of useful ideas
+  * elaborate on ideas that teammates contributed
+  * drop ones own idea in favor of an idea that someone wants to develop
+  * refuse to let an idea drop before everyone understands it
+  * resist time pressure and take time to listen when other people explain their ideas
+  * test ideas contributed by other people
+  * withhold quick critics of teammates ideas in order to keep the ideas flowing
+  * when crisising an idea make sure you are clear you are critiquing thr idea not the person
+  * test your own ideas before offering them
+  * when time and labor are short stop working on new ideas and just pitch in
+  * encourage team to drop ideas that succeeded earlier but will not any longer because of change
+  * revive a dropped idea when it will now work because of change
+  * does the idea add to, reduce, or keep the same the amount of toil on the project? (Can it be refined to be automatable? But sure, simple things that don’t scale have a place too... just maybe not if you’be scaled) (-RW)
+  * is it an idea that didn’t use to work, but now can? (-RW)}
 
 # lessons form a task oriented style
 
+@quote-highlight[
+	#:title       "Becoming a technical leader"
+	#:author      "Gerald Weinburg"
+	#:page-number 1624
+	#:url         "https://www.amazon.com/Becoming-Technical-Leader-Gerald-Weinberg-ebook/dp/B004J4VV3I/ref=sr_1_1?crid=FHTWINGGX9HB&dchild=1&keywords=becoming+a+technical+leader&qid=1597625292&sprefix=becoming+a+techical+leader%2Caps%2C668&sr=8-1"]{lessons form a task oriented style:
 
-> lessons form a task oriented style:
-> 1. when survival is concerned, there is no choice but to put people first
-> 2. if the job isn’t highly technical, the leader need not be competent, but can lead by fear
-> 3. people with strong technical backgrounds can convert any task into a technical task, thus avoiding doing work they don’t want to know
-> 4. leaders wh don’t care about people don’t have anyone to lead, unless their followers don’t have a choice
-> 5. no amount of caring for people will hold your audience if you have nothing to offer but pretend you do
-> 6. task oriented leaders tend to overestimate their own accomplishments
-> 7. very little work we do is really so important that it justifies sacrificing the future possibilities of the people doing the work
-> 8. when the work is complete, no leader can be absolutely sure that plans won’t go "aft algey”
-> 9. to be a successful problem solving leader, yoloorvqnizationi u fmust keep everyone’s humanness at the fore
-> 10. If you are a leader, the people are your work. there is no other work worth doing.
-> 
-> - From Becoming a technical leader by Gerald Weinburg on page 1624 (https://www.amazon.com/Becoming-Technical-Leader-Gerald-Weinberg-ebook/dp/B004J4VV3I/ref=sr_1_1?crid=FHTWINGGX9HB&dchild=1&keywords=becoming+a+technical+leader&qid=1597625292&sprefix=becoming+a+techical+leader%2Caps%2C668&sr=8-1)
+  1. when survival is concerned, there is no choice but to put people first
+  2. if the job isn’t highly technical, the leader need not be competent, but can lead by fear
+  3. people with strong technical backgrounds can convert any task into a technical task, thus avoiding doing work they don’t want to know
+  4. leaders wh don’t care about people don’t have anyone to lead, unless their followers don’t have a choice
+  5. no amount of caring for people will hold your audience if you have nothing to offer but pretend you do
+  6. task oriented leaders tend to overestimate their own accomplishments
+  7. very little work we do is really so important that it justifies sacrificing the future possibilities of the people doing the work
+  8. when the work is complete, no leader can be absolutely sure that plans won’t go "aft algey”
+  9. to be a successful problem solving leader, yoloorvqnizationi u fmust keep everyone’s humanness at the fore
+  10. If you are a leader, the people are your work. there is no other work worth doing.}
 
-
-> three essential fucnctions of a problem solving leadership:
-> 1. defininv the problem
-> 2. managing the flow of ideas
-> 3. controllng the quality
-> 
-> - From Becoming a technical leader by Gerald Weinburg on page 2729 ()
+@quote-highlight[
+	#:title       "Becoming a technical leader"
+	#:author      "Gerald Weinburg"
+	#:page-number 2729]{three essential fucnctions of a problem solving leadership:
+  1. defininv the problem
+  2. managing the flow of ideas
+  3. controllng the quality}
 
 
 # leadership considerations
